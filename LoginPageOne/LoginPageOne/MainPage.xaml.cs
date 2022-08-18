92af36c53179f2d3cc123254844387636beb3bc8
Hala-Sowery <hala.sowery.98@gmail.com>
2022-08-19 00:45:46 +0300

    login page

---
+++ LoginPageOne/LoginPageOne/MainPage.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace LoginPageOne
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            BindingContext = new MainViewModel();
+            InitializeComponent();
+        }
+
+        async void Button_Clicked(System.Object sender, System.EventArgs e)
+        {
+            await Navigation.PushAsync(new MyPage());
+        }
+
+        //void OnImageNameTapped(object sender, EventArgs args)
+        //{
+        //    try
+        //    {
+        //        DisplayAlert("Facebook", "You clicked on facebook image", "OK");
+        //    }
+        //    catch (Exception ex)
+        //    {
+        //        throw ex;
+        //    }
+        //}
+    }
+}
+
---
+++ LoginPageOne/LoginPageOne/MainViewModel.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.ComponentModel;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using Xamarin.Forms;
+
+namespace LoginPageOne
+{
+    public class MainViewModel : INotifyPropertyChanged
+    {
+        public Command LoginCommand { get; set; }
+
+        private bool isBusy;
+        public bool IsBusy
+        {
+            get => isBusy;
+            set
+            {
+                isBusy = value;
+                OnPropertyChanged();
+                LoginCommand?.ChangeCanExecute();
+
+            }
+        }
+
+        public MainViewModel()
+        {
+
+            LoginCommand = new Command(async () => await LoginAsync(), () => !IsBusy);
+
+        }
+        private async Task LoginAsync()
+        {
+            try
+            {
+
+                IsBusy = true;
+
+                await Task.Delay(2000);
+                if (CheckEmpty())
+                {
+                    if (name == "admin")
+                    {
+                        this.LoginCommand = new Command(async () =>
+                        {
+
+                            await Application.Current.MainPage.Navigation.PushAsync(new MyPage());
+                        });
+                    }
+                }
+                else
+                {
+                    await Application.Current.MainPage.DisplayAlert("Error", "Entries can not be empty", "Ok");
+
+                }
+
+            }
+            finally
+            {
+                IsBusy = false;
+            }
+        }
+
+
+        private bool CheckEmpty()
+        {
+
+            if ((string.IsNullOrEmpty(Name)) || (string.IsNullOrEmpty(Pass)))
+            {
+                return false;
+
+            }
+            return true;
+        }
+
+        string name = string.Empty;
+        public string Name
+        {
+            get => name;
+
+            set
+            {
+                if (name == value)
+                    return;
+                name = value;
+                OnPropertyChanged(nameof(Name));
+            }
+        }
+        string pass = string.Empty;
+        public string Pass
+        {
+            get => pass;
+
+            set
+            {
+                if (pass == value)
+                    return;
+                pass = value;
+                OnPropertyChanged(nameof(Pass));
+            }
+        }
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        void OnPropertyChanged(string name = "")
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+        }
+    }
+}
+
